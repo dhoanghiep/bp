d40dda80c86db2be552adb66c6d41d0f92d7c671
Hiep Dang <hiep.h1tv@gmail.com>
2022-06-07 20:39:12 +0700

    Update BED_handling.py
    Fix reverseComplement: add 'N'
    Edit hg19.pickle path
    Fix BedFile.importBEDFile: compatible with iCLIP.csv
    
    Add new class: ExperimentDataset

--- genome_preprocess/BED_handling.py
+++ genome_preprocess/BED_handling.py
@@ -16,10 +16,12 @@ def reverseComplement(string):
             complement_string += 'C'
         elif nu == 'C':
             complement_string += 'G'
+        elif nu == 'N':
+            complement_string += 'N'
     return complement_string[::-1]
 
 
-def openHg19Pickle(pickle_path = '../hg19/hg19.pickle'):
+def openHg19Pickle(pickle_path = 'hg19/hg19.pickle'):
     global chromosome_sequences
     with open(pickle_path, 'rb') as f:
         chromosome_sequences = pickle.load(f)
@@ -45,14 +47,11 @@ class BedFile():
         file_extension = file_path[-3:].lower()
         if file_extension == 'bed':
             data = pd.read_csv(file_path, sep = '\t', header = None)
+            data = data.drop(3, axis = 1)
         elif file_extension == 'csv':
             data = pd.read_csv(file_path, sep= ',',header = None)
-            data = data.drop(0, axis = 0)
-        id_column = 3   
-        data = data.drop([id_column],axis = 1)
         data.columns = ['chr','start','end','other','strand']
         self.data = data
-
 
     def getSequence(self,padding = 0, offset = 0, side = 'all_span', span = 0):
         global chromosome_sequences
@@ -94,3 +93,71 @@ class BedFile():
                 sequences.append(reverseComplement(extracted_sequence))
             
         return sequences
+
+
+class ExperimentDataset(BedFile):
+    def __init__(self,dataset_path):
+        super().__init__(dataset_path)
+
+    def makeAnnotatedData(self,window_size):
+        data = pd.DataFrame()
+        data['chromosome'] =self.data['chr']
+        data['sequence'] = self.getSequence(side = '3_prime',span = window_size)
+        data['annotation'] = self.getAnnotation(window_size)
+        return data
+
+    def getAnnotation(self,window_size):
+        annotations = []
+        for i in range(self.data.shape[0]):
+            annotation = [0]*window_size
+            branch_point_distance_to_3_prime = self.getDistanceTo3Prime(index = i)
+                      
+            if branch_point_distance_to_3_prime <= window_size:
+                annotation[-branch_point_distance_to_3_prime] = 1
+            else:
+                annotation = [branch_point_distance_to_3_prime]
+            annotations.append(np.array(annotation))
+        return annotations
+       
+    def getDistanceTo3Prime(self, index):
+        strand = self.data['strand'][index]
+        start = self.data['start'][index]
+        end = self.data['end'][index]
+        bp = self.data['other'][index]
+        
+        if strand == '+':
+            distance_to_3_prime = end - bp 
+        elif strand == '-':
+            distance_to_3_prime = bp - start + 1        
+
+        return distance_to_3_prime
+
+    def makeDereplicatedData(self,window_size):
+        if 'annotated_data' not in self.__dir__():
+            self.annotated_data = self.makeAnnotatedData(window_size)
+        
+        data = self.annotated_data
+        dereplicated_sequences = [''] # pseudo-sequence
+        merged_annotations = [[None]] # pseudo-annotation
+        chromosome = []
+        for i in range(data.shape[0]):
+            main_sequence = dereplicated_sequences[-1]
+            current_sequence = data['sequence'][i]
+            current_annotation = data['annotation'][i]
+            current_chromosome = data['chromosome'][i]
+
+            if current_sequence != main_sequence and len(current_annotation) != 1:
+                dereplicated_sequences.append(current_sequence)
+                merged_annotations.append(current_annotation)
+                chromosome.append(current_chromosome)
+            elif current_sequence == main_sequence and len(current_annotation) !=1:
+                merged_annotations[-1] = merged_annotations[-1] + current_annotation
+            else:
+                continue
+
+        dereplicated_sequences = dereplicated_sequences[1:] # remove pseudo-sequence
+        merged_annotations = merged_annotations[1:] # remove pseudo-annotation
+
+        return pd.DataFrame({'chromosome':chromosome,
+                             'sequence':dereplicated_sequences,
+                             'annotation':merged_annotations})
